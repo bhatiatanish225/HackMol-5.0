777da66867412b6904abc8b838d0c60c8e08f8d1
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-02-24 23:51:25 +0530

    Added Schema

--- backend/model/User.js
+++ backend/model/User.js
@@ -8,7 +8,24 @@ const userSchema=new Schema({
         required:true},
     
     email: {
-
-    }
+        type:String,
+        required:true,
+        unique:true
     },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    bankaccount:{
+        type:Number,
+        required:true
+       },
+    riskfactor:{
+        type:Number,
+        required:true
+    }
+    }
 );
+export default mongoose.model("User",userSchema);
+
