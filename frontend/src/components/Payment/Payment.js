a2ce9c042243f993e7176c862f71d7457c49f09e
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-02-24 16:45:29 +0530

    onclick

--- frontend/src/components/Payment/Payment.js
+++ frontend/src/components/Payment/Payment.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
 const Payment = () => {
+    const navigate = useNavigate();
+    const [UPIId,setUPIId]= useState('');
+
+    const handleChange=(e)=>{
+        setUPIId(e.target.value);
+    }
+    const handleNextButton = ()=>{
+        localStorage.setItem("UPIId",UPIId);
+        navigate("/enteramount");
+    }
+    console.log(UPIId);
   return (
     <div>
          <Box
@@ -20,8 +32,8 @@ const Payment = () => {
       borderRadius={5}
     >
         <Typography sx={{color:'#08456f'}} margin="normal">Enter the UPI ID</Typography>
-      <TextField margin='normal' label="UPI ID" id="fullWidth"  />
-      <Button sx={{background:'#189ec8', color:'white'}}>Add Amount</Button>
+      <TextField onChange={handleChange} margin='normal' label="UPI ID" id="fullWidth"  />
+      <Button onClick={handleNextButton} sx={{background:'#189ec8', color:'white'}}>Add Amount</Button>
     </Box>
    
        
