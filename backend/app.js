799c122464fcbf02db1dd0fbc79abe730feba6a5
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-02-25 00:51:03 +0530

    Signup Data

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import router from './routes/user-routes.js';
 
 const app=express();
+app.use(express.json());
+app.use("/api/user",router)
 mongoose
 .connect(
   "mongodb+srv://mokshmahajan2004:jkWkUvQtUpsOpcOk@cluster0.xcdoxoi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
