9270cb534d41624cfbc66935cbcafcc3e6b67962
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-02-25 00:37:57 +0530

    integrate

--- frontend/src/components/Payment/PaymentDone.js
+++ frontend/src/components/Payment/PaymentDone.js
@@ -1,7 +1,8 @@
 import { Box, Button, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import DoneAllIcon from '@mui/icons-material/DoneAll';
 const PaymentDone = () => {
+ 
   return (
     <>
     <Box
