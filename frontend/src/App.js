b4ac887969429a4fd5b21465fc8413ec1941b4b8
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-02-24 15:35:05 +0530

    Balance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,10 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard'
 import React from 'react';
-import Auth from './components/Balance/Auth.js';
-import Balance from './components/Balance/Balance.js';
 import Payment from './components/Payment/Payment';
 import EnterAmount from './components/Payment/EnterAmount';
-
+import Auth from './components/Balance/auth';
+import Balance from './components/Balance/Balance';
 // import Header from './components/Header';
 function App() {
   return <React.Fragment>
@@ -18,11 +17,10 @@ function App() {
     <main>
     <Routes>
       <Route path='/' element={<Dashboard/>}></Route>
-      <Route path='/auth' element={<Auth/>}></Route>
-      <Route path='/balance'element={<Balance/>}></Route>
       <Route path='/payment' element={<Payment/>}></Route>
       <Route path='/enteramount' element={<EnterAmount/>}></Route>
-
+      <Route path='/auth' element={<Auth/>}></Route>
+      <Route path='/balance' element={<Balance/>}></Route>
 
      
       
@@ -31,4 +29,4 @@ function App() {
 </React.Fragment>
 }
 
-export default App;
+export default App;
