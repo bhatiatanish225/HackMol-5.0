b714c4ac580816282200e5628f40e9476d2e513f
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-02-24 14:04:21 +0530

    Balance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard'
 import React from 'react';
+import Auth from './components/Balance/Auth.js';
+import Balance from './components/Balance/Balance.js';
+
 // import Header from './components/Header';
 function App() {
   return <React.Fragment>
@@ -13,6 +16,8 @@ function App() {
     <main>
     <Routes>
       <Route path='/' element={<Dashboard/>}></Route>
+      <Route path='/auth' element={<Auth/>}></Route>
+      <Route path='/balance'element={<Balance/>}></Route>
 
 
      
